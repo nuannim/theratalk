dc815dc709050bd8488c3d004678520fc3a0c5fe
ENdDoWn <folkza1212123@hotmail.com>
2025-07-04 18:33:20 +0700

    temp

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+import whisper
+import shutil
+import os
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+model = whisper.load_model("tiny")
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/stt", response_class=HTMLResponse)
+async def stt(request: Request, file: UploadFile = File(...)):
+    file_location = os.path.join(UPLOAD_FOLDER, file.filename)
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    result = model.transcribe(file_location, language="th")
+    text = result["text"]
+
+    os.remove(file_location)
+
+    return templates.TemplateResponse("index.html", {"request": request, "transcript": text})
